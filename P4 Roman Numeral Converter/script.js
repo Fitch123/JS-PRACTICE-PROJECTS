3a468cd452389a529c845472b9a1e5fcc1fff71d
Cesar <cfitchf@wgu.edu>
2025-10-11 00:25:34 -0700

    Added object

--- P4 Roman Numeral Converter/script.js
+++ P4 Roman Numeral Converter/script.js
@@ -1,1 +1,22 @@
 const input = document.getElementById("input-area");
+
+const romanNumerals = [
+    {value: 1000, symbol:"M"},
+    {value: 500, symbol:"D"},
+    {value: 100, symbol:"C"},
+    {value: 50, symbol:"L"},
+    {value: 10, symbol:"X"},
+    {value: 5, symbol:"V"},
+    {value: 1, symbol:"I"},
+    //SPECIAL CASES:
+    {value: 900, symbol:"CM"},
+    {value: 400, symbol:"CD"},
+    {value: 90, symbol:"XC"},
+    {value: 40, symbol:"XL"},
+    {value: 9, symbol:"IX"},
+    {value: 4, symbol:"IV"},
+];
+
+
+
+convertBtn.addEventListener("click", )
